20e3ef2748ac3151e93e2e449d64913832281fe9
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 10:48:26 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+
+import "./App.css";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+// TODO: Find better solution
+const API_URL = (process.env.NODE_ENV === 'development')
+  ? 'http://localhost:8080/api/users'
+  : 'https://kids-toyss.herokuapp.com/api/users';
 
 function App() {
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const data = await fetch(API_URL);
+        const dataJson = await data.json();
+
+        setUsers(dataJson);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getUsers();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {users?.map(user =>
+        <div key={user.id}>
+          <p>USER ID: {user.id}</p>
+          <p>USER NAME: {user.name}</p>
+          <p>USER EMAIL: {user.email}</p>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default App;
